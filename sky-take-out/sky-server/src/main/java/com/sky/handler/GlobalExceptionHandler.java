6edbb5b390125adedf352f22e47023b478daffca
felix <felixwindsor3344@gmail.com>
2025-12-07 13:55:12 +0800

    feat(员工管理): 实现新增员工功能
    - 在EmployeeService接口中添加save方法
    - 实现EmployeeServiceImpl中的save方法，包括密码加密和默认值设置
    - 在EmployeeMapper中添加insert方法
    - 添加EmployeeDTO数据传输对象
    - 在MessageConstant中添加ALREADY_EXISTS常量
    - 在GlobalExceptionHandler中添加对重复员工的异常处理
    - 在EmployeeController中添加save接口并添加Swagger注解
    - 更新JwtTokenAdminInterceptor设置当前用户ID
    - 修改application-dev.yml中的数据库密码配置

--- sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+
+import java.sql.SQLClientInfoException;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,6 +26,19 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLClientInfoException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            String username = split[2].replace("'","");
+            String msg = username+MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
     }
 
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,6 +12,10 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -39,7 +46,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // 对前端传递的密码进行md5加密，然后再进行比对
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -52,6 +60,32 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        //对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO, employee);
+        // 设置默认状态为启用 默认正常状态 1
+        employee.setStatus(StatusConstant.ENABLE); // 默认启用
+        // 设置默认密码为 123456，需要加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes())); // 密码加密
+
+        // 设置当前记录的创建时间和更新时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        
+        // 设置当前记录的创建人和更新人
+        employee.setCreateUser(BaseContext.getCurrentId());// 假设默认创建人ID为10
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.insert(employee);
+
     }
 
 }
